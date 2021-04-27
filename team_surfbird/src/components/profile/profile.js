ca99464dea206ffbc3b50ec635048e95ae654ba9
jonathanreaban17 <jonathanreaban17@augustana.edu>
2021-04-27 18:10:17 -0500

    Multiple Pages Works!!!

---
+++ team_surfbird/src/components/profile/profile.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div className="profile">
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        Hello World
+      </div>
+    );
+  }
+}
+export default Profile;

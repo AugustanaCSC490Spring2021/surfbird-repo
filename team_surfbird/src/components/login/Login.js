a5029dfbdd114cc8336aa6411291dc6a14793200
Wesley <wesleypulver17@augustana.edu>
2021-05-13 14:39:05 -0500

    fixed signup/login

--- team_surfbird/src/components/login/Login.js
+++ team_surfbird/src/components/login/Login.js
@@ -1,99 +1,130 @@
-import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import {React, Component} from "react";
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { withStyles, createStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
 import { firebaseApp } from "./../../firebase";
 
-//code modeled after https://www.bennettdocs.com/react-login-with-google-firebase/
+
+const styles = theme => ({
+    paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    },
+    avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(3),
+    },
+    submit: {
+    margin: theme.spacing(3, 0, 2),
+    },
+});
+
+
 class Login extends Component {
-  constructor(props) {
-    super(props);
-    this.login = this.login.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.signup = this.signup.bind(this);
-    this.state = {
-      email: "",
-      password: "",
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.login = this.login.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.state = {
+            email: "",
+            password: "",
 
-  handleChange(e) {
-    this.setState({ [e.target.name]: e.target.value });
-  }
+        };  
+    }
 
-  login(e) {
-    e.preventDefault();
-    firebaseApp
-      .auth()
-      .signInWithEmailAndPassword(this.state.email, this.state.password)
-      .then((u) => {})
-      .catch((error) => {
-        console.log(error);
+    handleChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+      }
+      
+      login(e) {
+        e.preventDefault();
+        firebaseApp
+          .auth()
+          .signInWithEmailAndPassword(this.state.email, this.state.password)
+          .then((u) => {})
+          .catch((error) => {
+            console.log(error);
       });
   }
-
-  signup(e) {
-    e.preventDefault();
-    firebaseApp
-      .auth()
-      .createUserWithEmailAndPassword(this.state.email, this.state.password)
-      .then((u) => {})
-      .then((u) => {
-        console.log(u);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }
-
-  render() {
-    return (
-      <div className="col-md-6">
-        <form>
-          <br />
-          <br />
-          <br />
-          <br />
-          <div class="form-group">
-            <label for="exampleInputEmail1">Email address</label>
-            <input
-              value={this.state.email}
-              onChange={this.handleChange}
-              type="email"
-              name="email"
-              class="form-control"
-              id="exampleInputEmail1"
-              aria-describedby="emailHelp"
-              placeholder="Enter email"
-            />
-            <small id="emailHelp" class="form-text text-muted">
-              We'll never share your email with anyone else.
-            </small>
-          </div>
-          <div class="form-group">
-            <label for="exampleInputPassword1">Password</label>
-            <input
-              value={this.state.password}
-              onChange={this.handleChange}
-              type="password"
-              name="password"
-              class="form-control"
-              id="exampleInputPassword1"
-              placeholder="Password"
-            />
-          </div>
-          <button type="submit" onClick={this.login} class="btn btn-primary">
-            Login
-          </button>
-          <button
-            onClick={this.signup}
-            style={{ marginLeft: "25px" }}
-            className="btn btn-success"
-          >
-            Signup
-          </button>
-        </form>
-      </div>
-    );
-  }
+    
+  
+    render() {
+        const { classes } = this.props;
+        return (
+            <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar}>
+                <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                Log In
+                </Typography>
+                <form className={classes.form} noValidate>
+                <Grid container spacing={2}>
+            
+                    <Grid item xs={12}>
+                    <TextField
+                        variant="outlined"
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                        defaultValue={this.state.email}
+                        onChange={this.handleChange}
+                    />
+                    </Grid>
+                    <Grid item xs={12}>
+                    <TextField
+                        variant="outlined"
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="current-password"
+                        defaultValue={this.state.password}
+                        onChange={this.handleChange}
+                    />
+                    </Grid>
+                </Grid>
+                <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    color="primary"
+                    className={classes.submit}
+                    onClick={this.login}
+                >
+                    Log In
+                </Button>
+   
+                </form>
+            </div>
+            <Box mt={5}>
+                {/* <Copyright /> */}
+            </Box>
+            </Container>
+        );
+    }
 }
-
-export default Login;
+export default withStyles(styles, { withTheme: true })(Login);;
